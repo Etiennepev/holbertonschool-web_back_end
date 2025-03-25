b1a1a7386d5c288c91f7b0298932099c4467a014
Etiennepev <etienneloichot@gmail.com>
2025-03-25 10:25:26 +0100

    fix 8 10 11 12

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -2,12 +2,12 @@ export default function createReportObject(employeesList) {
   const allEmployees = {};
   // eslint-disable-next-line
   for (const department in employeesList) {
-	allEmployees[department] = employeesList[department];
+    allEmployees[department] = employeesList[department];
   }
   return {
-    allEmployees: { ...employeesList },
-	getNumberOfDepartments(employees) {
-	  return Object.keys(employees).length;
-	}
+    allEmployees: {...employeesList },
+    getNumberOfDepartments(employees) {
+      return Object.keys(employees).length;
+    },
   };
 }
