93d2813784c11e8f0b17c3243e5f84c046a8300f
Etiennepev <etienneloichot@gmail.com>
2025-03-26 15:22:40 +0100

    adding spaces ? t4

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -5,27 +5,33 @@ export default class Pricing {
     this.amount = amount,
     this.currency = currency
   }
+  
   get amount() {
     return this._amount;
   }
+
   set amount(value) {
     if (typeof value !== 'number') {
       throw new Error('amount must be a number')
     }
     this._amount = value;
   }
+
   get currency() {
     return this._currency;
   }
+
   set currency(value) {
     if (!(value instanceof Currency)) {
       throw new Error('currency must be an instance of Currency')
     }
     this._currency = value;
   }
+
   displayFullPrice() {
-    return `${this.amount} ${this.currency.name} (${this.currency.code})`
+    return `${this._amount} ${this._currency.name} (${this.currency.code})`
   }
+
   static convertPrice(amount, conversionRate) {
     if (typeof amount !== 'number') {
       throw new error('amount must be a number')
