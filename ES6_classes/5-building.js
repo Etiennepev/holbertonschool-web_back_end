d769a13d51275228b2b33f95c05c383856243989
Etiennepev <etienneloichot@gmail.com>
2025-03-26 15:15:49 +0100

    fix t5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,17 +1,19 @@
 export default class Building {
   constructor(sqft) {
     if (typeof sqft !== 'number') {
-      throw new Error('sqft must be a number');
+      throw TypeError('sqft must be a number');
     }
     this._sqft = sqft;
     if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
   }
+
   get sqft() {
     return this._sqft;
   }
+
   evacuationWarningMessage() {
-    return `Evacuate ${this._floors} floors slowly`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
