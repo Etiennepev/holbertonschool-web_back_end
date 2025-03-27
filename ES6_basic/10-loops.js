c2ef2eeb7704e160c8bdfb0600567379cfef27cd
Etiennepev <etienneloichot@gmail.com>
2025-03-27 13:09:19 +0100

    fix t10

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -2,7 +2,7 @@ export default function appendToEachArrayValue(array, appendString) {
   let idx = 0;
   for (const value of array) {
     array[idx] = appendString + value;
-    idx++
+    idx++;
   }
 
   return array;
