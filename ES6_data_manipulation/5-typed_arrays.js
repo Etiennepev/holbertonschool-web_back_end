dd40cd93d76abe93044cdc526433eb736bb724a9
Etiennepev <etienneloichot@gmail.com>
2025-03-31 16:08:06 +0200

    task 5, 6, 7

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,9 @@
+export default function createInt8TypedArray(lenght, position, value) {
+  const buffer = new ArrayBuffer(lenght);
+  const view = new DataView(buffer);
+  if (position < 0 || position >= lenght) {
+    throw new Error('Position outside range');
+  }
+  view.setUint8(position, value);
+  return view;
+}
