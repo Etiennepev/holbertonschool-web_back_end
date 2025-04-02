d2adcb0aca3ace96076a7303507c59ec957b159b
Etiennepev <etienneloichot@gmail.com>
2025-04-02 10:43:11 +0200

    fix t6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,8 +1,9 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignUp(firstName, lastName, fileName) {
-  const picture = uploadPhoto(firstName, lastName);
-  const user = signUpUser(fileName);
-  return Promise.allSettled([picture, user]);
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
 }
