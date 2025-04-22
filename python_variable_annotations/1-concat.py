92625a44db9a5fdad1fc12565b558b31f178136c
Etiennepev <etienneloichot@gmail.com>
2025-04-22 11:32:49 +0200

    fix 0 1 ? and push 3

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 def concat(str1: str, str2: str) -> str:
-	"""Returns the concatenation of two strings."""
-	return str1 + str2
+    """Returns the concatenation of two strings."""
+    return str1 + str2
