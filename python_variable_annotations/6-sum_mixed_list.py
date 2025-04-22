727b10873febfa0a7bdec98899578bb11a2a8bb7
Etiennepev <etienneloichot@gmail.com>
2025-04-22 12:16:01 +0200

    FIX 5 6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 def sum_mixed_list(mxd_lst: list[int | float]) -> float:
-	"""Returns the sum of a mixed list of integers and floats."""
-	return sum(mxd_lst)
+    """Returns the sum of a mixed list of integers and floats."""
+    return sum(mxd_lst)
