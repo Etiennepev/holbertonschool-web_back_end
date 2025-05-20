9d43497173da83b6a9fd3a4f242d441f7381b9e8
Etiennepev <etienneloichot@gmail.com>
2025-05-20 10:27:34 +0200

    t4 t5

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+const app = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (method === 'GET' && url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (method === 'GET' && url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2])
+      .then((data) => {
+        res.end(data);
+      })
+    .catch((error) => {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end(error.message);
+    });
+  } else {
+  res.writeHead(404, { 'Content-Type': 'text/plain' });
+  res.end('Not Found');
+  }
+});
+
+app.listen(1245)
+module.exports = app;
