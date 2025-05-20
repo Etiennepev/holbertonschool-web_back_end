4ab9209022d30be7efdcff807d2cb6a0e29d810e
Etiennepev <etienneloichot@gmail.com>
2025-05-20 10:31:10 +0200

    fix

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,8 +1,9 @@
 const http = require('http');
+
 const countStudents = require('./3-read_file_async');
+
 const app = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
+  const { url, method } = req;
 
   if (method === 'GET' && url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -14,15 +15,15 @@ const app = http.createServer((req, res) => {
       .then((data) => {
         res.end(data);
       })
-    .catch((error) => {
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end(error.message);
-    });
+      .catch((error) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end(error.message);
+      });
   } else {
-  res.writeHead(404, { 'Content-Type': 'text/plain' });
-  res.end('Not Found');
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
   }
 });
 
-app.listen(1245)
+app.listen(1245);
 module.exports = app;
